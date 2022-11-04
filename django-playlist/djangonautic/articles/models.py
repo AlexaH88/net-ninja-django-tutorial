6fdbce7913fa68d36085ce8ec4d814a494ffff3a
Alexa Hendry <alexahendry88@gmail.com>
2022-11-04 11:50:15 +0000

    models

--- django-playlist/djangonautic/articles/models.py
+++ django-playlist/djangonautic/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# inherit basic functionality with the class Model from django's models method 
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    # slug is url address of article
+    slug = models.SlugField()
+    body = models.TextField()
+    # time and date is automatically added thanks to parameter
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later    
