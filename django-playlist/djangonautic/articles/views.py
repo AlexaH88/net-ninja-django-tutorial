dc25c2509c604090f748a8cf2aeb2cff9b6755eb
Alexa Hendry <alexahendry88@gmail.com>
2022-11-04 15:39:20 +0000

    template tags

--- django-playlist/djangonautic/articles/views.py
+++ django-playlist/djangonautic/articles/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Article
 
 
 # Create your views here.
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    # grab all the records from the Article database table and order by date
+    articles = Article.objects.all().order_by('date')
+    # add articles to the render properties below as a dictionary
+    return render(
+        request, 'articles/article_list.html', {'articles': articles}
+        )
